0c4b5470aa09dc3c96fccb2afde01ab9f7d74348
ShantNarkizian <32500380+ShantNarkizian@users.noreply.github.com>
2018-05-17 18:34:12 -0700

    Update GameScene.swift

--- GameScene.swift
+++ GameScene.swift
@@ -19,16 +19,13 @@ class GameScene: SKScene {
     let PlayButton = SKSpriteNode(imageNamed: "play")
     let bestscore = SKLabelNode()
     
-    
-    
-    
+
     override func didMove(to view: SKView) {
         self.PlayButton.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
         PlayButton.size = CGSize(width: 600, height: 600)
         self.addChild(PlayButton)
         self.backgroundColor = UIColor.white
-        
-        
+
         
         let highscoreDefault = UserDefaults.standard            //saves high score to core data
         
@@ -36,22 +33,18 @@ class GameScene: SKScene {
             highscore = highscoreDefault.value(forKey: "highscore") as! NSInteger!
         }
 
-        
         self.bestscore.fontName = "Helvetica Neue UltraLight"
         self.bestscore.text = "\(highscore)"
         self.bestscore.fontSize = 100
         self.bestscore.position = CGPoint(x: 0, y: -530)
         self.bestscore.fontColor = UIColor.black
         self.addChild(bestscore)
-        bestscore.zPosition = 0.1
-        
-        
+        bestscore.zPosition = 0.1   
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         var change = false
-        
-        
+               
         let touch = touches.first!
         if PlayButton.contains(touch.location(in: self)){
             print("touched")
@@ -62,13 +55,6 @@ class GameScene: SKScene {
             let NewScene = Play(fileNamed: "Play")
             NewScene?.scaleMode = .aspectFill
             self.view?.presentScene(NewScene!)
-        }
-        
+        }      
     }
-    
 }
-
-
-
-
-
